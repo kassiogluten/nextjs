b8c663d03a813020d3af1785cc855725f940b392
Kassio <kassiogluten@gmail.com>
2021-08-24 22:23:08 -0300

    custom 404

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,18 @@ import React from "react";
 //import { useMyContext } from "../contexts/Context";
 
 export function Hero() {
-//  const { isModalOpen } = useMyContext();
+  //  const { isModalOpen } = useMyContext();
   return (
     <Box align="center" w="100%" bg="blue.100">
-      <Flex pt={100} 
-        px="1rem"
+      <Flex
+        p="5rem 1rem"
         align="center"
         maxW={1200}
         bg="blue.50"
         justify="space-between"
         h={700}
         flexDir={{ base: "column", sm: "row" }}
+        //flexDir="column"
       >
         <Heading>Texto</Heading>
         <Heading>Texto</Heading>
